2537d2c330cbea00242eccbd9c890b9cc4633ff2
Marilokms <42029758+Marilok@users.noreply.github.com>
2025-06-26 12:33:55 +0200

    error handling

--- src/utils/createSupabaseClient.ts
+++ src/utils/createSupabaseClient.ts
@@ -10,6 +10,12 @@ const supabaseConfig: SupabaseClientConfig = {
   anonKey: import.meta.env.VITE_SUPABASE_ANON_KEY!,
 };
 
+if (!supabaseConfig.url || !supabaseConfig.anonKey) {
+  throw new Error(
+    "Supabase URL and anon key must be defined in environment variables."
+  );
+}
+
 const supabase = createClient(supabaseConfig.url, supabaseConfig.anonKey);
 
 export const createSupabaseClient = () => {
