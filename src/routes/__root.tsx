d2ffcb97da3fd76b553200f59ba16d751faf6cc6
Marilokms <42029758+Marilok@users.noreply.github.com>
2025-07-06 14:41:19 +0200

    deeplpink handler

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,7 +10,7 @@ export const Route = createRootRoute({
     const navigate = useNavigate();
     useEffect(() => {
       setupDeepLinkHandler(navigate);
-    }, [navigate]);
+    }, []);
 
     return (
       <AppTemplate>
