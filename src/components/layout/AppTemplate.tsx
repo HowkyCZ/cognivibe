a810eda702522e13992972f811f741fc66d41862
Marilokms <42029758+Marilok@users.noreply.github.com>
2025-07-06 14:41:57 +0200

    navigate using router

--- src/components/layout/AppTemplate.tsx
+++ src/components/layout/AppTemplate.tsx
@@ -4,10 +4,22 @@ import "../../App.css";
 interface AppTemplateProps {
   children: React.ReactNode;
 }
+import type { NavigateOptions, ToOptions } from "@tanstack/react-router";
+import { useRouter } from "@tanstack/react-router";
+
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    routerOptions: Omit<NavigateOptions, keyof ToOptions>;
+  }
+}
 
 const AppTemplate: React.FC<AppTemplateProps> = ({ children }) => {
+  let router = useRouter();
+
   return (
-    <HeroUIProvider>
+    <HeroUIProvider
+      navigate={(to, options) => router.navigate({ to, ...options })}
+    >
       {children}
       <ToastProvider placement="top-center" toastOffset={40} />
     </HeroUIProvider>
